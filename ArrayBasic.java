2108a302a5f3609e534bf2ecc4fdad2f53388f36
GAUR1704 <gauravparate160@gmail.com>
2023-10-05 11:22:10 +0530

    first commit

---
+++ ArrayBasic.java
@@ -0,0 +1,18 @@
+package com.LoopProject;
+
+public class ArrayBasic {
+
+	public static void main(String[] args) {
+		int[] marks = new int[3];
+		marks[0] = 96; // chemistry
+		marks[1] = 98; // physics
+		marks[2] = 93; // English
+		
+		for (int i=0; i<=2; i++)
+		
+		System.out.println(marks[i]);
+		
+
+	}
+
+}
---
+++ SumOfElement.java
@@ -0,0 +1,17 @@
+package com.LoopProject;
+
+public class SumOfElement {
+
+	public static void main(String[] args) {
+		int my_array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sum = 0;
+
+		for (int i : my_array)
+			
+			sum += i;
+		
+		System.out.println("The Sum of All numbers is " + sum);
+
+	}
+
+}
